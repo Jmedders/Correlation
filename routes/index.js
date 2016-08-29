7bb1d4a71023851c7929e26d29f442e63b8bfac5
Jeff <medders.jeffrey@gmail.com>
2016-08-28 23:38:19 -0600

    precursory logic for major functionality of distance search

--- routes/index.js
+++ routes/index.js
@@ -14,21 +14,35 @@ router.get('/', function(req, res, next) {
 router.get('/api/users', function (req,res,next) {
   knex('users').then(function(data){
     for (var i = 0; i < data.length; i++) {
-      var userlatitude = data[1].latitude;
-      var userlongitude = data[1].longitude;
-      userlatitude = parseFloat(userlatitude).toFixed(6);
-      userlongitude = parseFloat(userlongitude).toFixed(6);
-      console.log(userlatitude, userlongitude);
+      var userlatitude = data[i].latitude;
+      var userlongitude = data[i].longitude;
+      userlatitude = parseFloat(userlatitude);
+      userlongitude = parseFloat(userlongitude);
+      // console.log(userlatitude, userlongitude);
 
-      // userlocation = new GeoPoint(userlatitude, userlongitude);
+      userlocation = new GeoPoint(userlatitude, userlongitude);
       // console.log(userlocation);
 
     };
-    userlocation = new GeoPoint(39.6242, -105.2620);
+    // console.log(typeof userlatitude, typeof userlongitude);
+    // // userlocation = new GeoPoint(39.6242, -105.2620);
     user2location = new GeoPoint(39.8915, -105.2839);
+    // console.log('line30 here', userlocation, user2location);
     var miles = userlocation.distanceTo(user2location);
-    console.log(miles.toFixed(1));
-    res.json(userlatitude);
+    if (miles < 50) return data[1];
+    else if(miles > 50){
+      console.log('no matches');
+      res.json(userlatitude);
+    }
+  }).then(function(data){
+    knex('users_bands').where('user_id', data.id)
+      .fullOuterJoin('bands', 'bands.id', 'users_bands.band_id').then(function(info) {
+        var arr = [];
+      for (var i = 0; i < info.length; i++) {
+        arr.push(info[i].name);
+      }
+      res.json(arr);
+    });
   });
 });
 module.exports = router;
