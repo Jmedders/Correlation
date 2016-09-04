be1b508f4629d9557244a1b0b7f47fabbbce4039
Jeff <medders.jeffrey@gmail.com>
2016-09-04 17:21:46 -0600

    starting jsonwebtoken

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 var express = require('express');
+var jwt = require('jsonwebtoken');
+var bearerToken = require('express-bearer-token');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+app.use(bearerToken());
 
 app.use(express.static('www'));
 
