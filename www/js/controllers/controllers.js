7f5f589a7f49e13626ce670e0fffd6090e9c73f9
Jeff <medders.jeffrey@gmail.com>
2016-09-06 23:17:53 -0600

    figured out what was wrong with chat and logout

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -1,35 +1,36 @@
 app.controller('mainController', ['$scope', '$http', 'MyService', '$location', '$window', '$rootScope', function($scope, $http, MyService, $location, $window, $rootScope){
 
-   // socket.on('join', function(msg){
-   //   $('#messages').append($('<li>').text(msg));
-   // });
 
   $scope.view = {};
   $scope.view.checkmessage = function(id){
+    console.log('hi im checkmessage');
     MyService.messageRooms(id).then(function(data){
       $rootScope.room = data.data[0]['roomname'];
     })
   }
-  MyService.findUsers().then(function (data){
-    $scope.view.users = data.data;
-    var usersobjs = $scope.view.users;
-    var mybands = $scope.view.mylistbands;
-    comparebands();
-    function comparebands(){
-      for (var i = 0; i < mybands.length; i++) {
-        for (var j = 0; j < usersobjs.length; j++) {
-          var usersbandslist = usersobjs[j]['bandlist']
-          for (var k = 0; k < usersbandslist.length; k++) {
-            if (mybands[i] == usersbandslist[k]) {
-              usersobjs[j]['count']++;
-              // console.log('hey match!!', usersobjs[j]['username'], usersobjs[j]['count'], usersbandslist[k]);
+  $scope.view.inquire = function(){
+    MyService.findUsers().then(function (data){
+      $scope.view.users = data.data;
+      var usersobjs = $scope.view.users;
+      var mybands = $scope.view.mylistbands;
+      comparebands();
+      function comparebands(){
+        for (var i = 0; i < mybands.length; i++) {
+          for (var j = 0; j < usersobjs.length; j++) {
+            var usersbandslist = usersobjs[j]['bandlist']
+            for (var k = 0; k < usersbandslist.length; k++) {
+              if (mybands[i] == usersbandslist[k]) {
+                usersobjs[j]['count']++;
+                // console.log('hey match!!', usersobjs[j]['username'], usersobjs[j]['count'], usersbandslist[k]);
+              }
             }
           }
         }
+        // console.log($scope.view.users);
       }
       // console.log($scope.view.users);
-    }
-  })
+    })
+  }
   $scope.view.grabchatusername = function(userid){
     // console.log($rootScope.user.id, userid);
     MyService.makeRoom($rootScope.user.id, userid).then(function(data){
@@ -91,6 +92,7 @@ app.controller('mainController', ['$scope', '$http', 'MyService', '$location', '
     })
   }
   $scope.view.logout = function() {
+    console.log('hi please work');
     localStorage.clear();
     $location.path('/');
     $window.location.reload();
