b3013109b9f02b83bbfd15e4335ce9675e43c360
Jeff <medders.jeffrey@gmail.com>
2016-09-06 22:50:19 -0600

    minor validation on friendform

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -10,30 +10,26 @@ app.controller('mainController', ['$scope', '$http', 'MyService', '$location', '
       $rootScope.room = data.data[0]['roomname'];
     })
   }
-  $scope.view.inquire = function(){
-    MyService.findUsers().then(function (data){
-      console.log($scope.view.inquirenum);
-      $scope.view.users = data.data;
-      var usersobjs = $scope.view.users;
-      var mybands = $scope.view.mylistbands;
-      comparebands();
-      function comparebands(){
-        for (var i = 0; i < mybands.length; i++) {
-          for (var j = 0; j < usersobjs.length; j++) {
-            var usersbandslist = usersobjs[j]['bandlist']
-            for (var k = 0; k < usersbandslist.length; k++) {
-              if (mybands[i] == usersbandslist[k]) {
-                usersobjs[j]['count']++;
-                // console.log('hey match!!', usersobjs[j]['username'], usersobjs[j]['count'], usersbandslist[k]);
-              }
+  MyService.findUsers().then(function (data){
+    $scope.view.users = data.data;
+    var usersobjs = $scope.view.users;
+    var mybands = $scope.view.mylistbands;
+    comparebands();
+    function comparebands(){
+      for (var i = 0; i < mybands.length; i++) {
+        for (var j = 0; j < usersobjs.length; j++) {
+          var usersbandslist = usersobjs[j]['bandlist']
+          for (var k = 0; k < usersbandslist.length; k++) {
+            if (mybands[i] == usersbandslist[k]) {
+              usersobjs[j]['count']++;
+              // console.log('hey match!!', usersobjs[j]['username'], usersobjs[j]['count'], usersbandslist[k]);
             }
           }
         }
-        // console.log($scope.view.users);
       }
-      console.log($scope.view.users);
-    })
-  }
+      // console.log($scope.view.users);
+    }
+  })
   $scope.view.grabchatusername = function(userid){
     // console.log($rootScope.user.id, userid);
     MyService.makeRoom($rootScope.user.id, userid).then(function(data){
