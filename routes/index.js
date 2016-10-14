fd127f72d52e427dbdf0b447c66544e8fea5c8c0
Jeff <medders.jeffrey@gmail.com>
2016-10-14 14:26:25 -0600

    heroku debug

--- routes/index.js
+++ routes/index.js
@@ -206,7 +206,7 @@ router.get('/api/users', function (req,res,next) {
         currentUser.bandlist.push(userbands[i][j]['name']);
       }
     }
-    console.log('line 208: 'wrapArr);
+    console.log('line 208: ', wrapArr);
     res.json(wrapArr)
   }).catch(function(err){
     console.log(err)
